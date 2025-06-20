e12af06fe812ed51cf1c54f1903ac75147b8ff47
AmanSharma0577 <as731941@gmail.com>
2025-06-20 15:27:17 +0530

    Structured Layout

--- Js/Scripts.js
+++ Js/Scripts.js
@@ -1,13 +1,13 @@
 
 
-  fetch('navbar.html')
+  fetch('/Reusable/Navbar.html')
     .then(res => res.text())
     .then(data => {
       document.getElementById('nimbus-navbar-placeholder').innerHTML = data;
     })
     .catch(err => console.error("Navbar load error:", err));
 
- fetch('Footer.html')
+ fetch('/Reusable/Footer.html')
     .then(res => res.text())
     .then(data => {
       document.getElementById('nimbus-Footer-placeholder').innerHTML = data;
